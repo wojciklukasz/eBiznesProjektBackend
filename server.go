cf5f324fac5d24d9ee25fb67bd86f041079169d4
Łukasz Wójcik <35641278+wojciklukasz@users.noreply.github.com>
2022-06-25 22:51:44 +0200

    Update CORS policy

--- server.go
+++ server.go
@@ -12,10 +12,10 @@ func main() {
 	database.Connect()
 
 	e := echo.New()
-	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins: []string{"http://localhost:3000", "https://ebiznesprojekt.azurewebsites.net:3000"},
-	//}))
-	e.Use(middleware.CORS())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", "https://ebiznesprojekt.azurewebsites.net"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
